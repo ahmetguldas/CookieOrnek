bdee9fef879d7561d72f096ef65a6031117c81a7
ahmet güldaş <ahmet.guldas@hotmail.com>
2021-08-29 00:43:52 +0300

    Add project files.

---
+++ CookieOrnek/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using CookieOrnek.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieOrnek.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string ad)
+        {
+            Response.Cookies.Append("ad",ad, new CookieOptions() { Expires = DateTime.Now.AddDays(1)});//expires sona erme demek 1 gun sonra cookie silinecek
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Unut()
+        {
+            Response.Cookies.Delete("ad");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
